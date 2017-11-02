33a16e1fc9df9dca7a86658aa404de6455f6d70e
Ananileaf <411787945@qq.com>
2017-11-02 21:29:03 +0800

    :hammer: Class(Station) Api(recent_contests) add return(page && page_size), document updated.

--- application/controllers/Station.php
+++ application/controllers/Station.php
@@ -33,9 +33,34 @@ class Station extends CI_Controller {
 	public function recent_contests()
 	{
 
+		//config
+		$members = array('page_size', 'page');
+
 		$url = "http://contests.acmicpc.info/contests.json";
 		$content = file_get_contents($url); 	
 		$data = (array)json_decode($content);
+
+		//get page && page_size
+		try
+		{
+
+			//get post
+			if ($this->input->get('page_size') && $this->input->get('page'))
+			{
+				$post['page_size'] = $this->input->get('page_size');
+				$post['page'] = $this->input->get('page');
+				$data['page_size'] = $post['page_size'];
+				$data['page'] = $post['page'];
+			}
+
+		}
+
+		catch(Exception $e)
+		{
+			output_data($e->getCode(), $e->getMessage(), array());
+			return;
+		}
+
 		output_data(1, '获取成功', $data);
 
 	}
--- docs/WonderLand ApiDoc - Station.md
+++ docs/WonderLand ApiDoc - Station.md
@@ -8,6 +8,7 @@
 
 | 日期         | 备注  
 | ------------ | ------
+| **17/11/02** | **【recent_contest】 新增元素返回字段 page 、page_size**
 | **17/07/12** | 添加接口 · **【获取近期比赛 · recent_contest】**
  
 ---
@@ -18,12 +19,20 @@
 
 - **接口网址：http://icpc-system.and-who.cn/Station/recent_contests**
 
+| **`GET` 字段可选项** | 备注
+| --------------- | --------
+| **page_size**   | 设置分页大小，和 **page** 成对存在
+| **page**        | 设置查询页，和 **page_size** 成对存在
+
+- **查询示例：http://icpc-system.and-who.cn/Station/recent_contests?page_size=2&page=2**
 - **成功返回例子**
 
 ```
 {
 	"type": 1,
 	"message": "获取成功",
+	"page_size": "2",
+	"page": "2",
 	"data": [
 		{
 			"id": "88904",
@@ -46,3 +55,7 @@
 	]
 }
 ```
+
+
+
+
