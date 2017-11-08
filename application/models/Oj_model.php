eb8219e832b8ef19a71253628321b0b52e6e9775
Mystarset <850069892@qq.com>
2017-11-08 10:13:44 +0800

    :sparkles: Model(Oj_model) add get_cf_acproblems.

--- application/models/Oj_model.php
+++ application/models/Oj_model.php
@@ -91,12 +91,15 @@ class Oj_model extends CI_Model {
 		$content = curl_exec($ch3);
 		curl_close($ch3);
 
+		//删除cookie文件
+		unlink($cookie_file);
+
 		$re = "/".$form['OJusername']."/";
 		if ( ! preg_match($re,$content,$match))
 		{
 			throw new Exception("关联账户用户名或密码错误");
 		}
-		unlink($cookie_file);
+
 		$this->db->insert('oj_account',filter($form, $members));
 	}
 
@@ -229,6 +232,7 @@ class Oj_model extends CI_Model {
 		$content = curl_exec($ch3);
 		curl_close($ch3);
 
+		//删除cookie文件
 		unlink($cookie_file);
 		//正则匹配获取判断是否登录成功
 		$re = "/".$form["OJusername"]."/i";
@@ -373,6 +377,114 @@ class Oj_model extends CI_Model {
 			throw new Exception("用户名错误");
 		}
 		curl_close($ch);
+
+		return $res;
+	}
+
+	/**
+	 * 获取cf过题数
+	 */
+	public function get_cf_acproblems($form)
+	{
+		//config
+		$members = array('Uusername', 'OJname');
+
+		//check token
+		$this->load->model('User_model','user');
+		if (isset($form['Utoken']))
+		{
+			$this->user->check_token($form['Utoken']);
+		}
+
+		//check OJname
+		if (isset($form['OJname']))
+		{
+			if ($form['OJname'] != 'cf')
+			{
+				throw new Exception('oj名称错误');
+			}
+		}
+		else
+		{
+			throw new Exception('oj名称错误');
+		}
+
+		//get OJusername & OJpassword
+		$OJuser = $this->db->select(array('OJusername','OJpassword'))
+						->where(array('OJname' => $form['OJname'],
+									'Uusername' => $form['Uusername']))
+						->get('oj_account')->result_array();
+		if (! $OJuser)
+		{
+			throw new Exception('用户名错误');
+		}
+		//get url
+		$url ='http://codeforces.com/enter?back=%2Fproblemset%2Fstandings';
+			
+		//get csrf_token and cookie
+		$ch1 = curl_init();
+		$cookie_file = dirname(__FILE__).'/cookie.txt';
+		curl_setopt($ch1, CURLOPT_URL, $url);
+		curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch1, CURLOPT_COOKIEJAR, $cookie_file);
+		if (curl_exec($ch1))
+		{
+			$text = curl_multi_getcontent($ch1);
+		}
+		else
+		{
+			throw new Exception('用户名错误');
+		}
+		curl_close($ch1);
+
+		//正则匹配获取csrf_token
+		$re = "/<meta name=\"X-Csrf-Token\" content=\"(\w*)\"\/>/";
+		if (preg_match($re, $text, $match)){
+			$token = $match[1];
+		}
+		else
+		{
+			throw new Exception("用户名错误");
+		}	
+		
+		//模拟登录
+		$post_data = array
+					(
+						'csrf_token' => $token,
+						'action' => 'enter',
+						'handle' => $OJuser[0]['OJusername'],
+						'password' => $OJuser[0]['OJpassword']
+					);
+		$post_data = http_build_query($post_data);
+		$ch2 = curl_init();
+		curl_setopt($ch2, CURLOPT_URL, $url);
+		curl_setopt($ch2, CURLOPT_POST, 1);
+		curl_setopt($ch2, CURLOPT_POSTFIELDS, $post_data);
+		curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch2, CURLOPT_COOKIEFILE, $cookie_file);
+		curl_exec($ch2);
+		curl_close($ch2);
+
+		$url = "http://codeforces.com/problemset/standings";
+		$ch3 = curl_init();
+		curl_setopt($ch3, CURLOPT_URL, $url);
+		curl_setopt($ch3, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch3, CURLOPT_COOKIEFILE, $cookie_file);
+		$content = curl_exec($ch3);
+		curl_close($ch3);
+		
+		//删除cookie文件
+		unlink($cookie_file);
+		//正则匹配获取过题数信息
+		$re = "/".$OJuser[0]["OJusername"]."<\/a>\s+<\/td>\s+<td >\s+(\d+)/i";
+		if (preg_match($re, $content, $match))
+		{
+			$res = $match[1];
+		}
+		else
+		{
+			throw new Exception('用户名错误');
+		}
 
 		return $res;
 	}
